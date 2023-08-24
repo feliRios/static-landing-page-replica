25d85eaa2a782fa4542ac8532b8e70cdcfcb40d5
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-08-23 21:24:29 -0300

    Modificacion en la adaptacion a responsive del menu desplegable

--- js/main.js
+++ js/main.js
@@ -1,62 +1,7 @@
 // Necesarios para el navbar responsive
 
-window.addEventListener("load", () => {
-  const width = window.innerWidth;
-  const navbar = document.querySelector(".navbar-options");
-  if (width <= 1125) {
-    navbar.innerHTML = `
-                        <div class="menu-hamburguesa">
-                        <span class="menu-linea1 menu-linea"></span>
-                        <span class="menu-linea2 menu-linea"></span>
-                        <span class="menu-linea3 menu-linea"></span>
-                        </div>
-                       `;
-    const menu = document.querySelector(".menu-hamburguesa");
-    menu.addEventListener("click", toggleMenu);
-  } else {
-    navbar.innerHTML = `
-                      <ul>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">Pricing</a></li>
-                        <li><a href="#">Apps & Services</a></li>
-                        <li><a href="#">Enterprise</a></li>
-                        <li><a href="#">OEM</a></li>
-                        <li><a href="#">Help & Tutorials</a></li>
-                        <li><a href="#">Sign In</a></li>
-                        <li class="sign-up"><a href="" style="text-decoration: none; color: rgba(1,104,163,1);">Sign Up</a></li>
-                      </ul>
-                       `;
-  }
-});
-
-window.addEventListener("resize", () => {
-  const width = window.innerWidth;
-  const navbar = document.querySelector(".navbar-options");
-  if (width <= 1125) {
-    navbar.innerHTML = `
-                        <div class="menu-hamburguesa">
-                        <span class="menu-linea1 menu-linea"></span>
-                        <span class="menu-linea2 menu-linea"></span>
-                        <span class="menu-linea3 menu-linea"></span>
-                        </div>
-                       `;
-    const menu = document.querySelector(".menu-hamburguesa");
-    menu.addEventListener("click", toggleMenu);
-  } else {
-    navbar.innerHTML = `
-                      <ul>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">Pricing</a></li>
-                        <li><a href="#">Apps & Services</a></li>
-                        <li><a href="#">Enterprise</a></li>
-                        <li><a href="#">OEM</a></li>
-                        <li><a href="#">Help & Tutorials</a></li>
-                        <li><a href="#">Sign In</a></li>
-                        <li class="sign-up"><a href="" style="text-decoration: none; color: rgba(1,104,163,1);">Sign Up</a></li>
-                      </ul>
-                       `;
-  }
-});
+const menu = document.querySelector(".menu-hamburguesa");
+menu.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   // Animacion del icono
@@ -65,12 +10,5 @@ function toggleMenu() {
   document.querySelector(".menu-linea3").classList.toggle("menu-linea3-active");
 
   const navbarResponsive = document.querySelector("#nv-responsive");
-  navbarResponsive.classList.toggle("nv-oculto");
-
-  // Desplazamiento del menu
-  if (navbarResponsive.classList.contains('nv-oculto')) {
-    navbarResponsive.style.top = "-100%";
-  } else {
-    navbarResponsive.style.top = "0";
-  }
+  navbarResponsive.classList.toggle("nv-show");
 }
